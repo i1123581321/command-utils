819905e5923a9ece6d8427d55b360ea0db8dda98
i1123581321 <gokinjolno112358@gmail.com>
2023-11-04 16:43:17 +0800

    add convert quote

---
+++ src/text_utils/convert_quote.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from sys import stdin
+from typing import Optional
+
+import typer
+
+
+def convert_quote(filename: Optional[Path] = None, encoding: str = "utf-8") -> None:
+    if filename is not None:
+        content = filename.read_text(encoding=encoding)
+    else:
+        content = "\n".join([line for line in stdin])
+
+    content = (
+        content.replace("\u201c", "\u300c")
+        .replace("\u201d", "\u300d")
+        .replace("\u2018", "\u300e")
+        .replace("\u2019", "\u300f")
+    )
+
+    if filename is not None:
+        filename.write_text(content, encoding="utf-8")
+    else:
+        print(content)
+
+
+def main() -> None:
+    typer.run(convert_quote)
