adb9636f8fd3c52ff87adbc88dd76701402c6d9f
i1123581321 <gokinjolno112358@gmail.com>
2023-11-05 16:59:05 +0800

    update split text

--- src/text_utils/split_text.py
+++ src/text_utils/split_text.py
@@ -1,29 +1,33 @@
-import argparse
 from pathlib import Path
+from typing import Annotated
 
-parser = argparse.ArgumentParser(
-    prog="text-split",
-    description="split text file by line no",
-)
+import typer
 
-parser.add_argument("filename", type=str, help="input file path")
-parser.add_argument("lineno", nargs="+", type=int, help="line numbers")
+
+def split_text(
+    filename: Annotated[
+        Path,
+        typer.Argument(exists=True, dir_okay=False, resolve_path=True),
+    ],
+    lineno: list[int] = typer.Argument(default=None),
+) -> None:
+    lines = filename.read_text(encoding="utf-8").split("\n")
+    if len(lineno) == 0:
+        lineno = [i for i, line in enumerate(lines) if line.startswith("# ")]
+        lineno.remove(0)
+    else:
+        lineno = list(map(lambda x: x - 1, sorted(lineno)))
+    result: list[str] = []
+    start = 0
+    for i in lineno:
+        result.append("\n".join(lines[start:i]))
+        start = i
+    result.append("\n".join(lines[start:]))
+    for i, r in enumerate(result):
+        Path(f"{filename.stem}-{i + 1:0>2}{filename.suffix}").write_text(
+            r, encoding="utf-8"
+        )
 
 
 def main() -> None:
-    args = parser.parse_args()
-    filename = Path(args.filename)
-    lineno: list[int] = args.lineno
-    lineno = list(map(lambda x: x - 1, lineno))
-    lineno.sort()
-    result: list[str] = []
-    with open(filename, "r", encoding="utf-8") as f:
-        content = f.readlines()
-        start = 0
-        for i in lineno:
-            result.append("".join(content[start:i]))
-            start = i
-        result.append("".join(content[start:]))
-    name, ext = filename.stem, filename.suffix
-    for i, r in enumerate(result):
-        Path(f"{name}-{i + 1:02}{ext}").write_text(r, encoding="utf-8")
+    typer.run(split_text)
